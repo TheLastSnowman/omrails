eeb2e5a47905aa5693a909687d66016c7d1076aa
Jake Schlingman <jake.schlingman@appfolio.com>
2017-12-05 09:23:39 -0800

    Reverting sign out fix to see if sign in will work again on heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery3
-//= require jquery_ujs
-//= require turbolinks
 //= require popper
 //= require bootstrap-sprockets
+//= require jquery_ujs
+//= require turbolinks
 //= require_tree .
